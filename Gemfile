a050899246d48502d67abe15b427801cccad977a
Artem <yegorov0725@yandex.ru>
2019-01-24 17:41:39 +0300

    Add devise

--- Gemfile
+++ Gemfile
@@ -38,7 +38,7 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
-
+gem 'devise'
 gem 'babosa'
 gem 'friendly_id', '~> 5.2.0'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,25 @@
 #
 # Table name: users
 #
-#  id         :bigint(8)        not null, primary key
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                     :bigint(8)        not null, primary key
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :inet
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :inet
+#  name                   :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 
 require 'rails_helper'
@@ -18,12 +33,10 @@ RSpec.describe User, type: :model do
   end
 
   it "must present email" do
-    pending "TODO"
     expect(user.email).to be_present
   end
 
   it "must present avatar" do
-    pending "TODO"
     expect(user.avatar).to be_present
   end
 
