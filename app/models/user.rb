4c79291a5ad6254cfbe31467ec895677982272f4
Artem <yegorov0725@yandex.ru>
2019-08-29 17:00:45 +0300

    Merge fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable
-  has_many :pages, foreign_key: 'author_id'
-  has_many :posts, foreign_key: 'author_id'
+  has_many :pages, foreign_key: 'author_id', dependent: :destroy
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
 
   has_one_attached :avatar
 end
